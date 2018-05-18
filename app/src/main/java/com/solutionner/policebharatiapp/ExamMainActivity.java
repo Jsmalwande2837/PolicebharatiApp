0a32a291e4de3e63023302d295ca1b21e682bfff
Jiteshs2837 <malwandejitesh@gmail.com>
2018-05-18 20:54:15 +0530

    Third commit

---
+++ app/src/main/java/com/solutionner/policebharatiapp/ExamMainActivity.java
@@ -0,0 +1,13 @@
+package com.solutionner.policebharatiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_main);
+    }
+}
