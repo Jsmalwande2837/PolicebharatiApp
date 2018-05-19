8d67441387e5076a323b07fa0fe2cdde7fbb7b4e
Jiteshs2837 <malwandejitesh@gmail.com>
2018-05-19 15:36:40 +0530

    Fourth commit

--- app/src/main/java/com/solutionner/policebharatiapp/LoginActivity.java
+++ app/src/main/java/com/solutionner/policebharatiapp/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.solutionner.policebharatiapp;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,4 +30,39 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
         finish();
     }
+
+    @OnClick(R.id.txtForgotPass)
+    public void ForgotPassword() {
+        dialog = new Dialog(LoginActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.alert_for_forgotpassword);
+        dialog.show();
+
+        final EditText edtMobileNo = (EditText) dialog.findViewById(R.id.edtMobileNo);
+        final Button textViewCancel = (Button) dialog.findViewById(R.id.textViewCancel);
+        final Button textViewOk = (Button) dialog.findViewById(R.id.textViewOk);
+
+        dialog.setCancelable(false);
+
+        textViewCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        textViewOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtMobileNo.getText().length() < 1) {
+                    edtMobileNo.setError("Enter Mobile no");
+                } else {
+                    String mobileNo = edtMobileNo.getText().toString();
+                    dialog.dismiss();
+                }
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/solutionner/policebharatiapp/RecordActivity.java
+++ app/src/main/java/com/solutionner/policebharatiapp/RecordActivity.java
@@ -1,9 +1,11 @@
 package com.solutionner.policebharatiapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -11,12 +13,20 @@ import butterknife.OnClick;
 
 public class RecordActivity extends AppCompatActivity {
 
+    @InjectView(R.id.textviewtry)
+    TextView textviewtry;
+    @InjectView(R.id.textviewRight)
+    TextView textviewRight;
+    @InjectView(R.id.textviewWrong)
+    TextView textviewWrong;
+    @InjectView(R.id.textviewUnsolve)
+    TextView textviewUnsolve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
-
+        ButterKnife.inject(this);
 
     }
 
